bebbb5d9eeb2b272a8e7a335d9125121318a7489
matjaz tome <matjaz.tome@fe.uni-lj.si>
2020-04-07 14:54:38 +0200

    Comands.c created
    Comands.c created

---
+++ RespiratorApp/Comands.c
@@ -0,0 +1,18 @@
+/*
+ * Comands.c
+ *
+ * Created: 7. 04. 2020 13:44:28
+ *  Author: matja
+ */ 
+
+#include "Comands.h"
+
+const comand_params_t device_comands[COMANDS_NBR] = {
+											{COM_MODE, 'M', PARAM_CHAR, 0, 0},
+											{COM_RAMPUP, 'R', PARAM_U8, 50, 20},
+											{COM_INSPIRATION_TIME, 'I', PARAM_U16, 10, 5000},
+											{COM_EXPIRATION_TIME, 'E', PARAM_U16, 10, 5000},
+											{COM_VOLUME, 'V', PARAM_U16, 100, 1000},
+											{COM_BREATH_RATE, 'A', PARAM_U8, 1, 20},
+											{COM_PEEP, 'P', PARAM_U8, 0, 20}		
+									   };
---
+++ RespiratorApp/Comands.h
@@ -0,0 +1,48 @@
+/*
+ * Comands.h
+ *
+ * Created: 7. 04. 2020 13:44:45
+ *  Author: matja
+ */ 
+
+#include <stdint.h>
+
+#define COMANDS_NBR 7
+
+typedef enum {
+				COM_MODE,
+				COM_RAMPUP,
+				COM_INSPIRATION_TIME,
+				COM_EXPIRATION_TIME,
+				COM_VOLUME,
+				COM_BREATH_RATE,
+				COM_PEEP
+			}comand_list_t;
+			
+typedef enum {
+			 DEV_SET_P,
+			 DEV_SET_I,
+			 DEV_SET_D,
+			 DEV_SET_MOTOR
+			}dev_comand_list_t;
+			 
+typedef enum{
+			PARAM_CHAR,
+			PARAM_U8,
+			PARAM_S8,
+			PARAM_U16,
+			PARAM_S16,
+			PARAM_U32,
+			PARAM_S32
+			}param_type_t;			 
+
+			
+			
+			
+typedef struct comand_params {
+								comand_list_t comand;
+								char comand_char;
+								param_type_t param_type;
+								uint32_t param_min;
+								uint32_t param_max;
+							}comand_params_t;
